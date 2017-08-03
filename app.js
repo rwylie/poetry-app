b8b344ae69f0a3ea05352777a0d8e1b3c3ae0907
Ronda Wylie <rondawylie@yahoo.com>
2017-08-03 11:05:53 -0500

    new welcome banner and poems saved in postgres

--- app.js
+++ app.js
@@ -8,58 +8,18 @@ var promise = require('bluebird');
 var pgp = require('pg-promise')({
   promiseLib: Promise
 });
-var session = require('express-session');
-var morgan = require('morgan');
 
-// var pbkdf2 = require('pbkdf2');
-// var crypto = require('crypto');
-//
-// var salt = crypto.randomBytes(20).toString('hex');
-// var password= "hello";
-// var key = pbkdf2.pbkdf2Sync(
-//   password, salt, 36000, 256, 'sha256'
-// );
-// var hash = key.toString('hex');
-
-app.use(morgan('dev'));
+var db = pgp(process.env.DATABASE_URL || {
+  host: "localhost",
+  port: 5432,
+  database: 'poetic',
+  user: 'postgres'
+});
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('view engine', 'hbs');
 app.use('/static', express.static('public'));
-
-//how do I set up SECRET_KEY?
-app.use(session({
-  secret: process.env.SECRET_KEY || 'dev',
-  resave: true,
-  saveUninitialized: false,
-  cookie: {maxAge: 600000}
-}));
-
-//asks for the user session, if session, goes next()
-//req.path
-app.use(function (req, res, next) {
-  if (req.session.user) {
-    next();
-  } else if (req.path == '/create/') {
-    res.redirect('/login');
-  } else {
-    next();
-  }
-});
-
-
-//changed api to frontend
-// app.get('/api', cache('5 minutes'), function (req, res, next) {
-//   console.log('Generating new response');
-//   axios.get(`http://words.bighugelabs.com/api/2/efc9deef165ea63a44d6f9205e00faf1/${req.query.word}/json`)
-//   .then(function (r) {
-//       res.json(r.data);
-//       console.log(r.data);
-//   })
-//   .catch(next);
-// });
-
 
 app.get('/', function(req, res) {
   res.render('home.hbs', {});
@@ -77,27 +37,20 @@ app.get('/create/', function(req, res) {
 app.get('/about/', function(req, res) {
   res.render('about.hbs');
 });
-/********* LOGIN **********/
 
-app.get('/login', function(req, res) {
-  res.render('login.hbs');
+app.post('/add_poem', function (req, res, next) {
+  var poem = req.body.createPoem1 + "\n" + req.body.createPoem2 + "\n" + req.body.createPoem3 + "\n" + req.body.createPoem4;
+  var user_id = req.body.uid;
+  db.none(`INSERT INTO Poems VALUES (default, '${poem}', '${user_id}')`)
+    .then(function() {
+      res.redirect('/saved');
+    })
+    .catch(next);
 });
 
-app.post('/login', function (req, res) {
-  var username = req.body.username;
-  var password = req.body.password;
-
-  if (username == 'ronda' && password == 'hello') {
-    req.session.user = username;
-    res.redirect('/');
-  } else {
-    res.redirect('login.hbs');
-  }
-});
-
-app.get('/logout', function (req, res) {
-  delete req.session;
-  res.redirect('/')
+app.get('/display_poems', function(req,res, next) {
+  var user_id = req.body.uid;
+  db.any('SELECT * FROM poems WHERE user_id = $1', user_id);
 })
 
 var PORT = process.env.PORT || 9090;
--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,3 +1,46 @@
+var User = {};
+function auth () {
+  return new Promise(function (resolve, reject) {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(function (result) {
+        User.user = result.user;
+        resolve(User);
+      })
+      .catch(function (e) {
+        reject(e);
+      });
+  });
+}
+
+firebase.auth()
+  .onAuthStateChanged(function(user) {
+    if (user) {
+      User.user = user;
+      console.log(user);
+      document.getElementById('loggedout').innerHTML =  User.user.displayName + " is a poetic genius!";
+      poemBy();
+    }
+  });
+
+function logout () {
+  firebase.auth().signOut().then(function() {
+    User = {};
+  }, function(error) {
+    // An error happened.
+  });
+}
+
+function loggedOut() {
+    document.getElementById('loggedout').innerHTML = "Goodbye. You are now logged out";
+}
+function poemBy() {
+  if (User)
+  {
+  document.getElementById('poem-by').innerHTML = User.user.displayName;
+  document.getElementById('uid').value = User.user.uid;
+  }
+}
 $(document).ready(function(){
      $('.parallax').parallax();
 // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
