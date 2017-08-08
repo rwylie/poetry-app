f436c5f2c389cfe824095bfc3e647fbfa7f09543
Ronda Wylie <rondawylie@yahoo.com>
2017-08-07 22:49:27 -0500

    fixed bug with print.css

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -74,6 +74,5 @@ $(document).ready(function(){
     // Show sideNav
     $('.button-collapse').sideNav('hide');
    $('.button-collapse').sideNav('show');
-   // Hide sideNav
-   $('.button-collapse').sideNav('hide');
+
 });
